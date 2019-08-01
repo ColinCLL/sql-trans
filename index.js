bef010f5312fa6465f7d9a39a34e18b703a57398
陈立林 <245839124@qq.com>
2019-08-01 18:06:07 +0800

    dev

---
+++ index.js
@@ -0,0 +1,56 @@
+let input= "select name, version form table where name = test"
+
+/**
+ * @param {String} input 
+ * @return {Array} tokens
+ */
+function tokenizer(input) {
+  let tokens = [], current = 0;
+
+  console.log(input.length)
+  while (current < input.length) {
+    let char = input[current];
+    // 匹配空字符和逗号, 空字符和逗号不重要可以忽略
+    const WHITESPACE = /[\s,']/;
+		if (WHITESPACE.test(char)) {
+			current++;
+			continue;
+    }
+    var LETTERS = /[a-z]/i;
+		if (LETTERS.test(char)) {
+			var value = "";
+
+			// 同样，我们用一个循环遍历所有的字母，把它们存入 value 中。
+			while (LETTERS.test(char)) {
+				value += char;
+				char = input[++current];
+			}
+
+			// 然后添加一个类型为 `word` 的 token，然后进入下一次循环。
+			tokens.push({
+				type: isKey(value) ? "key" : "word",
+				value: value
+			});
+
+			continue;
+    }
+    let OPERATOR = /[=\*/\+-]/
+    if (OPERATOR.test(char)) {
+      tokens.push({
+				type: "operator",
+				value: char
+      });
+      current++;
+    }
+    console.log(current)
+  }
+  return tokens
+}
+
+function isKey(word) {
+  let keys = ["select", "form", "where"]
+  return keys.indexOf(word) != -1;
+}
+
+let tokens = tokenizer(input)
+console.log(JSON.stringify(tokens, null, 2));
